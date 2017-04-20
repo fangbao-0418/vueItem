2daf8a866e5ecbe63709fa3ce08e8308e47db22a
fangbao <fangbao@wanglibank.com>
2017-04-20 13:26:53 +0800

    edit

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,25 +1,14 @@
 <template>
 <div class="view">
-  <div class="user">
-    <div class="user-avatar">
-      <img src="../imgs/avatar@80x80.png" />
-    </div>
-    <p class="user-nick">方宝</p>
-    <div class="user-remind">
-      <p>还有99999元体验金未领取 |</p>
-      <span>去领取</span>
-    </div>
-  </div>
+  <user-briefly-show></user-briefly-show>
 </div>
 </template>
 <script>
-import axios from 'axios'
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+import userBrieflyShow from '../components/user-briefly-show'
 export default {
   data () {
     return {
-      msg: process.env.NODE_ENV,
-      commentData: '222'
+      msg: process.env.NODE_ENV
     }
   },
   created () {
@@ -40,6 +29,9 @@ export default {
       console.log(res1, res2)
     })
   },
+  components: {
+    userBrieflyShow
+  },
   methods: {
     sub () {
       console.log(this.$refs.profile.innerText)
@@ -49,23 +41,5 @@ export default {
 }
 </script>
 <style lang="sass" scoped>
-.user
-  width: 100%
-  height: 12rem
-  .user-avatar
-    text-align: center
-    &:before
-      content: ''
-      display: block
-      margin-top: .46rem
-    img
-      margin: 0 auto
-      border-radius: 50%
-      width: .8rem
-      height: .8rem
-  .user-nick
-    background-color: red
-    font-size: .28rem
-    height: .4rem
-    line-height: .4rem
+
 </style>
