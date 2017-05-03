619f0c8420c31f9a4ceef12be68cc1007902602b
高燕茹 <962290336@qq.com>
2017-05-03 22:56:33 +0800

    edit

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
 function resolve (dir) {
   return path.resolve(__dirname, dir);
 }
-
 var isPro = process.env.NODE_ENV.trim() === 'production';
 
 var plugins = [
@@ -18,7 +17,7 @@ var plugins = [
   //给输出的文件头部添加注释信息。
   new webpack.BannerPlugin('This file is created by fangbao'),
   new ExtractTextPlugin({
-    filename: isPro ? './static/css/[name].[contenthash].css' : '[name].[contenthash].css',
+    filename: isPro ? 'css/[name].[contenthash].css' : '[name].[contenthash].css',
     //disable: false,
     allChunks: true
   }),
@@ -26,7 +25,7 @@ var plugins = [
     template: './src/index.html',
     // 要把<script>标签插入到页面哪个标签里(body|true|head|false)
     inject: 'body',
-
+    filename: resolve("dist/index.html"),
     // hash如果为true，将添加hash到所有包含的脚本和css文件，对于解除cache很有用
     // minify用于压缩html文件，其中的removeComments:true用于移除html中的注释，collapseWhitespace:true用于删除空白符与换行符
     removeComments:true,
@@ -62,9 +61,10 @@ if(isPro){
 module.exports = {
 	entry: resolve("src/app.js"),
 	output:{
-		path: resolve("dist"),
-    filename: isPro ? './static/js/[name].[hash].js' : '[name].[hash].js',
-    chunkFilename: isPro ? './static/js/[name].[chunkhash].js' : '[name].[chunkhash].js'
+		path: isPro ? resolve("dist/assets") : resolve("dist"),
+    publicPath: isPro ? "/assets/" : "/",
+    filename: isPro ? 'js/[name].[hash].js' : '[name].[hash].js',
+    chunkFilename: isPro ? 'js/[name].[chunkhash].js' : '[name].[chunkhash].js'
 	},
 	module:{
 		rules: [
@@ -133,7 +133,7 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: isPro ? './static/imgs/[name].[hash:7].[ext]' : '[name].[hash:7].[ext]'
+          name: isPro ? 'imgs/[name].[hash:7].[ext]' : '[name].[hash:7].[ext]'
         }
       },
       {
@@ -141,7 +141,7 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: isPro ? './static/fonts/[name].[hash:7].[ext]' : '[name].[hash:7].[ext]'
+          name: isPro ? 'fonts/[name].[hash:7].[ext]' : '[name].[hash:7].[ext]'
         }
       }
 		]
