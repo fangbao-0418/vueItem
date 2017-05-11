a2e47e56969919a75d85e2e3847f904569564208
fangbao <fangbao@wanglibank.com>
2017-05-11 10:09:14 +0800

    build

--- src/store/modules/bridge.js
+++ src/store/modules/bridge.js
@@ -1,5 +1,7 @@
 import * as types from '../mutation-types'
 import { wlb } from '../../util'
+import cookie from 'js-cookie'
+
 // initial state
 const state = {
   isApp: false,
@@ -15,6 +17,16 @@ const actions = {
     try {
       wlb.ready({
         app (mixins) {
+          alert('old' + JSON.stringify(cookie.get()))
+          mixins.sendUserInfo((result) => {
+            const sessionid = result.sessionid
+            const splitArray = sessionid.split(';')
+            for (let key in splitArray) {
+              let split = splitArray[key].split('=')
+              cookie.set(split[0], split[1], { path: '/', domain: '.wanglibao.com' })
+            }
+          })
+          alert('new' + JSON.stringify(cookie.get()))
           commit(types.FETCH_BRIDGE_INFO, { mixinList: mixins, isWeb: false, isApp: true })
           cb && cb()
         },
