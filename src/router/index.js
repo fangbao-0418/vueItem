403acc09498cdfad784874b9e555463781f0c77a
高燕茹 <962290336@qq.com>
2017-05-13 23:27:00 +0800

    build

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,37 @@
 import store from '../store'
-// import cookie from 'js-cookie'
-
-// import Index from '../views/index' // 普通路由加载
-// import TopicDetail from '../views/topicDetail'
-// import TopicAdd from '../views/TopicAdd'
-// import Person from '../views/person.vue'
-// import PersonInfoEdit from '../views/personInfoEdit'
-// import PersonNews from '../views/personNews'
-// import PersonTopics from '../views/personTopics'
-// import PersonTask from '../views/personTask'
-// import Activities from '../views/activityList'
-// import ActivityDetail from '../views/activityDetail'
 
 /**
- * 路由分类
+ * 普通路由和懒加载
+ * import Index from '../views/index' // 普通路由加载
  * const Index = resolve => require.ensure([], () => resolve(require('../views/index')), 'index')
  * const Index = resolve => require(['../views/index.vue'], resolve) // AMD风格代码分块语法
  * require.ensure 是 Webpack 的特殊语法，用来设置 code-split point
  * require.ensure的第一个参数为所需依赖 第三个参数为chunk命名
 **/
 
-require('babel-register')
+/* 普通路由 */
+import Index from '../views/index' // 普通路由加载
+import TopicDetail from '../views/topicDetail'
+import TopicAdd from '../views/TopicAdd'
+import Person from '../views/person.vue'
+import PersonInfoEdit from '../views/personInfoEdit'
+import PersonNews from '../views/personNews'
+import PersonTopics from '../views/personTopics'
+import PersonTask from '../views/personTask'
+import Activities from '../views/activityList'
+import ActivityDetail from '../views/activityDetail'
 
-const Index = resolve => require(['../views/index'], resolve)
-const TopicDetail = resolve => require(['../views/topicDetail'], resolve)
-const TopicAdd = resolve => require(['../views/TopicAdd'], resolve)
-const Person = resolve => require(['../views/person'], resolve)
-const PersonInfoEdit = resolve => require(['../views/personInfoEdit'], resolve)
-const PersonNews = resolve => require(['../views/personNews'], resolve)
-const PersonTopics = resolve => require(['../views/personTopics'], resolve)
-const PersonTask = resolve => require(['../views/personTask'], resolve)
-const Activities = resolve => require(['../views/activityList'], resolve)
-const ActivityDetail = resolve => require(['../views/activityDetail'], resolve)
+/* 懒加载路由 */
+// const Index = resolve => require(['../views/index'], resolve)
+// const TopicDetail = resolve => require(['../views/topicDetail'], resolve)
+// const TopicAdd = resolve => require(['../views/TopicAdd'], resolve)
+// const Person = resolve => require(['../views/person'], resolve)
+// const PersonInfoEdit = resolve => require(['../views/personInfoEdit'], resolve)
+// const PersonNews = resolve => require(['../views/personNews'], resolve)
+// const PersonTopics = resolve => require(['../views/personTopics'], resolve)
+// const PersonTask = resolve => require(['../views/personTask'], resolve)
+// const Activities = resolve => require(['../views/activityList'], resolve)
+// const ActivityDetail = resolve => require(['../views/activityDetail'], resolve)
 
 function requireAuth (to, from, next) {
   store.dispatch('fetchBridgeInfo', () => {
