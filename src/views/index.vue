2874e6a7fa1a189daabf7e09aabff92d047239e8
fangbao <fangbao@wanglibank.com>
2017-05-18 13:53:21 +0800

    build

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="view">
-  <wlb-header :options="{ title: '网利社区', rightConfigs: [{ type: 'share', options: { title: '网利社区' } }] }"></wlb-header>
+  <wlb-header :options="{ title: '网利社区', rightConfigs: [{ type: 'share', options: {} }] }"></wlb-header>
   <scroll-wrapper>
     <user-briefly-show></user-briefly-show>
     <div class="container mt-20">
